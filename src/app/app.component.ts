97878051370b5e563af558017bdfecad69203fc6
Klaas Cuvelier <klaas@showpad.com>
2023-11-27 12:02:02 +0100

    status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+} from '@angular/router';
+import { filter, map, switchMap } from 'rxjs';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, CommonModule],
   selector: 'lieven-leavin-root',
   styleUrl: './app.component.scss',
   template: `
     <img src="/assets/iphone.png" alt="" />
     <section>
-      <header>
+      <header [ngStyle]="statusBar()">
         <div>Showpad</div>
         <div>9:41</div>
       </header>
@@ -21,4 +29,23 @@ import { NxWelcomeComponent } from './nx-welcome.component';
     <a class="homebutton" routerLink="/"></a>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  private router = inject(Router);
+  private statusBar$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    map((event) => {
+      let route: ActivatedRoute = this.router.routerState.root;
+      let routeTitle = '';
+      while (route!.firstChild) {
+        route = route.firstChild;
+      }
+
+      return {
+        color: '#fff',
+        background: '#000',
+        ...route.snapshot.data['statusBar'],
+      };
+    }),
+  );
+  protected statusBar = toSignal(this.statusBar$);
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,30 @@ export const appRoutes: Route[] = [
     path: 'photos',
     loadComponent: () =>
       import('./photos/photos.container').then((m) => m.PhotosContainer),
+    children: [
+      {
+        path: 'list',
+        loadComponent: () =>
+          import('./photos/list.component').then((m) => m.PhotoListComponent),
+        data: {
+          statusBar: { background: '#fff', color: '#000' },
+        },
+      },
+      {
+        path: 'detail/:id',
+        loadComponent: () =>
+          import('./photos/detail.component').then(
+            (m) => m.PhotoDetailComponent,
+          ),
+        data: {
+          statusBar: { background: '#fefefe', color: '#000' },
+        },
+      },
+      {
+        path: '**',
+        redirectTo: 'list',
+      },
+    ],
   },
   {
     path: 'showpad',
@@ -27,16 +51,25 @@ export const appRoutes: Route[] = [
     path: 'slack',
     loadComponent: () =>
       import('./slack/slack.container').then((m) => m.SlackContainer),
+    data: {
+      statusBar: { background: '#000', color: '#fff' },
+    },
   },
   {
     path: 'safari',
     loadComponent: () =>
       import('./safari/safari.container').then((m) => m.SafariContainer),
+    data: {
+      statusBar: { background: '#fff', color: '#000' },
+    },
   },
   {
     path: 'mail',
     loadComponent: () =>
       import('./mail/mail.container').then((m) => m.MailContainer),
+    data: {
+      statusBar: { background: '#fefefe', color: '#000' },
+    },
   },
   { path: '**', redirectTo: 'home' },
 ];
