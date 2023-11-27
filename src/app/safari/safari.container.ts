8c72f764a4a11d3f2264ac7070b84ba2b5f14d06
Klaas Cuvelier <klaas@showpad.com>
2023-11-27 11:48:04 +0100

    safari

---
+++ src/app/safari/safari.container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'll-safari',
+  standalone: true,
+  styleUrl: './safari.container.scss',
+  template: ` <iframe src="https://planetreesoftware.com/"></iframe> `,
+})
+export class SafariContainer {}
