16e1cca45b36569faea11b600913690e8af94615
Klaas Cuvelier <klaas@showpad.com>
2023-11-27 13:33:36 +0100

    add pnt members

--- src/app/settings/settings.container.ts
+++ src/app/settings/settings.container.ts
@@ -14,11 +14,54 @@ import { Component } from '@angular/core';
       <h2>Thank you and goodbye</h2>
       <p>From all Showpad Product and Technology members:</p>
       <p>
-        {{ pntMembers }}
+        Adam Kufel, Adam Roszyk, Adam Taciak, Aditya Jain, Agnieszka Janiec,
+        Alexander Starosolsky, Ana de la Borbolla, Anton Marker, Aren Maes,
+        Arthur Petrie, Artur Pelenskyi, Aurelien Lemesre, Bart Callant, Bart
+        Hemmeryckx-Deleersnijder, Ben De Keersmaecker, Benjamin Lierman, Bert De
+        Geyter, Bert Holvoet, Bert Van Hauwaert, Bram D'hooghe, Bram De Geyter,
+        Bram Peirs, Brecht De Vlieger, Brecht François, Carl Bonsu, Cathleen
+        Heimberg, Cedric Lemaire, Cristina Dontu, Damian Bałut, Daming Du,
+        Darren Hearst, Davy Van Den Steen, Dawid Branicki, Debbie Rodrigues,
+        Dieter Lecluyse, Dimitri Van Heucke, Dmitry Kouznetsov, Dominique
+        Dierickx, Douglas Grigg, Drew Pountney, Efe Osazee, Elaine Mui, Emery
+        Walraet, Evan Smith, Femi Veys, Florian Landuyt, Ganeshan Manickam,
+        Gareth Moore, Gemma Huddy, Geoffrey Beertens, Gertjan Gaillet, Giel De
+        Bleser, Giel Vanden Herrewegen, Glenny De Cock, Gorka Salgado, Gregory
+        Lahousse, Hannes Swimberghe, Hao Sun, Harlan Surroz, Hendrik Isebaert,
+        Hidayat Halim, Ignacio Eguinoa, Illia Kovalov, Iryna Kravets, Iryna
+        Mocherniuk, Jabori Jackson, Jack Verstappen, Jacob Moy, Jakub Zawada,
+        Jarno Goossens, Jef Galle, Jef Van Hoyweghen, Jeffrey Van de Velde,
+        Jelle Nagels, Jeroen Minnaert, Jesse Dobbelaere, Jesse Fuller, Joe Mann,
+        Julian Hudson, Julie Harnisch, Julien Havet, Jurgen Schepens, Jurgen Van
+        de Moere, Kamil Khalil, Karolina Kojak, Kasia Sobótka, Katarzyna
+        Szymańska, Kelli Koschmann, Klaas Cuvelier, Klaudia Kwaśniak, Koen
+        Buckinx, Koen Francois, Koen Van Durme, Kris Boulez, Kyle Driscoll,
+        Kyriakos Vallianatos, Lajos Kossuth, Laurens Dewaele, Leen De Clercq,
+        Liesbeth de Keyzer, Lieven Dekeyser, Lorenz Van der Eecken, Louis
+        Beullens, Louis Jonckheere, Louise Strong, Lucas Argüello, Lucas
+        Rouckhout, Lucy Jackman, Lukas Reichert, Maarten Desnouck, Maciej
+        Doskocz, Madalina Cosma, Marco Stahlhacke, Marina Oliveira, Marine
+        Maillet, Marko Matijevic, Márton Rausch, Mary Hauber, Mathias Van Hecke,
+        Maxim Tarasov, Mehedi Anam Sarder, Merve Can, Michael De Schuyter,
+        Michael Meunier, Michaël Schauwers, Michael van Rantwijk, Michał Amurow,
+        Miguel Margodt, Mike Warner, Milan De Buck, Mohamed Abida, Mushfiq
+        Rahman, Nataliia Ukhanska, Nathan Jones, Nazar Kuradovets, Nela
+        Matijevic, Niall Tunmore, Nikolay Koleda, Nils Tijtgat, Nishanth Jeeru,
+        Oleg Merkulov, Pablo Frosi, Patrick Debois, Patrick Werz, Paul Anderson,
+        Paul Campbell, Peter Minne, Philippe Bujeau, Pierre Clotuche, Pieterjan
+        Criel (Pj3), Piotr Dzieszko, Pirashanth Uthayashankar, Radosław Luter,
+        Rafik George, Raphael Allegre, Razvan Mocanu, Robin Vercruysse, Rodrigo
+        Bravo, Roy Hollanders, Ruben De Freyne, Sam Neirinck, Sara Van Der
+        Straeten, Sara Vermeire, Senne Vieira, Simon Baeyens, Simon Bockaert,
+        Simon Dean, Stacie Loving, Stefan Vandegehuchte, Stepan Makara, Steven
+        Camps, Stijn Deschuymer, Stijn Dobbelaere, Stijn Haus, Tessa Watson,
+        Theofilos Tzachristas, Thijs Coolen, Thomas Hollevoet, Tim Ducheyne,
+        Tine Lucas, Tobias Burri, Tom Eyckmans, Tony Grout, Toon Lamberigts,
+        Vadym Horiainov, Victor Nogales, Vincent Kwok, Virginie Heloire,
+        Volodymyr Smuk, Wesley Vanbrabant, Wout Lagae, Wouter Durnez, Wouter
+        Smet, Xavier Geli, Xiaoqi Li, Xisco Sastre Cabot, Yuri Galiza Gama Lyra
       </p>
     </section>
   `,
 })
-export class SettingsContainer {
-  pntMembers = ['klaas cuvelier'].join(', ');
-}
+export class SettingsContainer {}
