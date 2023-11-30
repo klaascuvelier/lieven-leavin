647b07dc266fdd2e96a2ab14ff3a6f667b56b330
Klaas Cuvelier <klaas@showpad.com>
2023-11-30 09:50:05 +0100

    feat: email updates

--- src/app/mail/mail.container.ts
+++ src/app/mail/mail.container.ts
@@ -8,14 +8,41 @@ import { Component } from '@angular/core';
     <h1>Inbox</h1>
     <ol class="mails">
       <li class="mail">
-        <div class="unread"></div>
-        <div class="title">Goodbye</div>
-        <div class="excerpt">Hi Lieven, your email will be disconnected</div>
+        <div class="badge unread"></div>
+        <div class="title">Team Showpad sent you a GroupGreeting eCard!</div>
+        <div class="excerpt">
+          Hi Lieven, Team Showpad sent you a card. <a href="">View your card</a>
+        </div>
       </li>
 
       <li class="mail">
-        <div class="title">Offboarding complete</div>
-        <div class="excerpt">Sad sad sas.d</div>
+        <div class="title">Mailbox disconnected</div>
+        <div class="excerpt">
+          lieven.dekeyser&#64;showpad.com can no longer be used.
+        </div>
+      </li>
+
+      <li class="mail">
+        <div class="badge flag"></div>
+        <div class="title">Data limit exceeded</div>
+        <div class="excerpt">
+          Because of your data usage, you have an outstanding bill of
+          &euro;&nbsp;185.000. This amount needs to be paid in full
+          before&hellip;
+        </div>
+      </li>
+
+      <li class="mail">
+        <div class="title">Gitlab access revoked</div>
+        <div class="excerpt">
+          You no longer have access to the Showpad codebase.
+        </div>
+      </li>
+
+      <li class="mail">
+        <div class="badge unread"></div>
+        <div class="title">Goodbye</div>
+        <div class="excerpt">De groetjes 👋</div>
       </li>
     </ol>
   `,
